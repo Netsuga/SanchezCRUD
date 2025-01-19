b9d79c7d72c287c170df0ff97bc11cd210dea522
Netsuga <123356198+Netsuga@users.noreply.github.com>
2025-01-19 10:14:20 +0800

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from . import views
+from .views import (
+    StudentListView, StudentDetailView, StudentCreateView,
+    StudentUpdateView, StudentDeleteView
+)
 
 urlpatterns = [
-    path('', views.student_list, name='student_list'),
-    path('register/', views.student_register, name='student_register'),
+    path('', StudentListView.as_view(), name='student-list'),
+    path('create/', StudentCreateView.as_view(), name='student-create'),
+    path('<int:pk>/', StudentDetailView.as_view(), name='student-detail'),
+    path('<int:pk>/update/', StudentUpdateView.as_view(), name='student-update'),
+    path('<int:pk>/delete/', StudentDeleteView.as_view(), name='student-delete'),
 ]
