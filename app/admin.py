a461435669f1926818e6cb26112519ab00caa05a
Netsuga <123356198+Netsuga@users.noreply.github.com>
2025-01-19 10:12:50 +0800

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import Student
+# Register your models here.
 
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'age', 'grade', 'email', 'date_registered')
-    search_fields = ('first_name', 'last_name', 'email')
+admin.site.register(Student)
