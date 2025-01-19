0d16597fcd0916f1a045368f6db0c987b55c8843
Netsuga <123356198+Netsuga@users.noreply.github.com>
2025-01-19 10:13:24 +0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Student
+from django.core.exceptions import ValidationError
 
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['first_name', 'last_name', 'age', 'grade', 'email']
+        fields = ['name', 'age', 'grade', 'email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if Student.objects.filter(email=email).exists():
+            raise ValidationError('This email address is already registered.')
+        return email
