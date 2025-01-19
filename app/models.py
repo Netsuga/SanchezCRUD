fade95fb3852658912db6b3954dc8534223f4f24
Netsuga <123356198+Netsuga@users.noreply.github.com>
2025-01-19 10:13:55 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    age = models.IntegerField()
-    grade = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    grade = models.CharField(max_length=20)
     email = models.EmailField(unique=True)
-    date_registered = models.DateTimeField(auto_now_add=True)
+    registration_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.name
+
